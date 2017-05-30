a74b70fcf9d2d19a7db1228cb3dbdde913052d69
Eyal Silberman <eyal@200apps.co>
2017-05-30 15:14:53 +0300

    lets go

---
+++ CacheFS.cpp
@@ -0,0 +1,59 @@
+//
+//  CacheFS.cpp
+//  os4
+//
+//  Created by Eyal Silberman on 30/05/2017.
+//  Copyright © 2017 Eyal Silberman. All rights reserved.
+//
+
+#include "CacheFS.h"
+
+int CacheFS_init(int blocks_num, cache_algo_t cache_algo,
+                 double f_old , double f_new  )
+
+{
+    
+    
+    
+    
+    return 0;
+}
+
+
+int CacheFS_destroy()
+{
+    return 0;
+}
+
+
+int CacheFS_open(const char *pathname)
+{
+    
+    return 0;
+}
+
+int CacheFS_close(int file_id)
+{
+    
+    return 0;
+}
+
+int CacheFS_pread(int file_id, void *buf, size_t count, off_t offset)
+{
+    
+    return 0;
+}
+
+int CacheFS_print_cache (const char *log_path)
+{
+    
+    return 0;
+}
+
+
+int CacheFS_print_stat (const char *log_path)
+{
+    
+    return 0;
+}
+
---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  os4
+//
+//  Created by Eyal Silberman on 30/05/2017.
+//  Copyright © 2017 Eyal Silberman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
